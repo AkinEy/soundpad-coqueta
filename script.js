7020875ae01171c963b3211bdacb23e126a84652
AkinEy <akirussia1@gmail.com>
2025-08-02 20:39:08 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const sounds = [
+  { name: "Gülme", file: "sounds/example1.mp3" },
+  { name: "Alkış", file: "sounds/example2.mp3" }
+];
+
+const container = document.getElementById("buttons-container");
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("sound-button");
+  btn.innerText = sound.name;
+
+  const audio = new Audio(sound.file);
+
+  btn.addEventListener("click", () => {
+    audio.currentTime = 0;
+    audio.play();
+  });
+
+  container.appendChild(btn);
+});
